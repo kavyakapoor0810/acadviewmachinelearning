6a23deac9597c566afce75c2e3b7229ee89c780f
Kavya Kapoor <32263500+kavyakapoor0810@users.noreply.github.com>
2018-08-09 15:40:50 +0530

    2nd

---
+++ assi2.py
@@ -0,0 +1,10 @@
+#question1
+import numpy as np
+n=np.random.rand(10,1)
+print("mean: ",n.mean())
+
+#question2
+import numpy as np
+n=np.random.rand(20,1)
+print("Variance: ",n.var())
+print("Standard Diviation: ",n.std())
