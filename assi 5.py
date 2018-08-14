d883aee546ad0774a7ab3c8fec35a23f63f0591e
Kavya Kapoor <32263500+kavyakapoor0810@users.noreply.github.com>
2018-08-14 19:34:26 +0530

    5

--- assi 5.py
+++ assi 5.py
@@ -1,1 +1,14 @@
-print("hello")
+import pandas as pan
+#a
+data=pan.read_csv('assi5raw.csv')
+print(data.isnull().sum())
+#b
+data=data.dropna() #drop the missing values
+print(data.isnull().sum())
+#c
+data1=pan.read_csv('assi5raw.csv')#to fill missing with mean
+print(data1.fillna(data1.mean()))
+#d
+print(data1.dtypes)
+data3=pan.Series(data1=data.MaxTemp)
+print(data3)
