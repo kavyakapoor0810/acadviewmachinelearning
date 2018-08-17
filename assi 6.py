db20e1a249b0fd9228cadf9ec17ca426102ed9c4
Kavya Kapoor <32263500+kavyakapoor0810@users.noreply.github.com>
2018-08-17 14:29:53 +0530

    6

--- assi 6.py
+++ assi 6.py
@@ -1,14 +1,10 @@
-import pandas as pan
-#a
-data=pan.read_csv('assi5raw.csv')
-print(data.isnull().sum())
-#b
-data=data.dropna() #drop the missing values
-print(data.isnull().sum())
-#c
-data1=pan.read_csv('assi5raw.csv')#to fill missing with mean
-print(data1.fillna(data1.mean()))
-#d
-print(data1.dtypes)
-data3=pan.Series(data1=data.MaxTemp)
-print(data3)
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("https://raw.githubusercontent.com/Shreyas3108/Weather/master/weather.csv ")
+data = data.dropna()
+sns.distplot(data['Rainfall'], bins= 20)
+sns.kdeplot(data['Sunshine'], shade=True)
+sns.distplot(data['WindGustSpeed'], hist=False)
+sns.countplot(data['RainToday'])
+sns.boxplot(data['MaxTemp'])
